f76e42b5abe493609f119d2bc16465474cad6883
Benjamin Elliott <benjamin.elliott.be@gmail.com>
2023-06-22 12:30:10 -0400

    let's go

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './components/Header.js';
+import Navbar from './components/Navbar.js';
+import Main from './components/Main.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
-      Homepage
+    <Header>
+    </Header>
+    <Navbar>
+    </Navbar>
+    <Main></Main>
+    <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div>
+            <img src='..assets/Logo.svg' atl="default"></img>
+        <h1>This is the header</h1>
+        </div>
+        
+    );
+}
+
+export default Header;
