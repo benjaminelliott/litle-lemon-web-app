5740185b2294a4cf58d2d7c4dc98fa90abdcdca7
Benjamin Elliott <benjamin.elliott.be@gmail.com>
2023-06-28 21:57:35 -0400

    finished form

--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -25,6 +25,15 @@ import {
 
 export default function BookingPage() {
 
+    const validate = value => {
+
+        let errorMessage;
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+          errorMessage = 'Invalid email address';
+        }
+        return errorMessage;
+      };
+
     return (
         
         <Container as="section" className="booking" pl="125px"  pr="125px" pt="50px" pb="50px">
@@ -32,68 +41,94 @@ export default function BookingPage() {
                 <Formik
                 initialValues={
                     {
-                        firstName: "First name",
-                        lastName: "last name",
-                        email: "Email address",
+                        firstName: "",
+                        lastName: "",
+                        email: "",
                         date: new Date(),
                         time: "",
                         guests: ""
                     }}
                     onSubmit={async (values) => {
                         await new Promise((resolve) => setTimeout(resolve, 500));
-                        alert(JSON.stringify(values, null, 2));
+                        alert("Booking not confirmed");
                     }}
                 >
-                    <Form>
-                        <VStack>
-                            <HStack>
-                                <VStack alignItems="flex-start">
+                    {({ errors, touched }) => (
+                    <Form display="flex">
+                        <VStack display="flex">
+                            <HStack flexGrow={1} min-width="100%">
+                                <VStack alignItems="flex-start" min-width="50%">
                                     <label htmlFor="firstName">First Name</label>
                                     <Field name="firstName" type="text"/>
                                 </VStack>
-                                <VStack alignItems="flex-start">
+                                <VStack alignItems="flex-start" min-width="50%">
                                     <label htmlFor="lastName">Last Name</label>
                                     <Field name="lastName" type="text"/>
                                 </VStack>
-                            </HStack>
-                            <VStack alignItems="flex-start">
+                                <VStack alignItems="flex-start" width="100%">
                                 <label htmlFor="email">Email address</label>
-                                <Field name="email" type="email" min-width="100%" />
+                                <Field validate={validate} name="email" type="email" width="100%" />
+                                {errors.email && touched.email ? <div>{errors.email}</div> : null}
                             </VStack>
+                            </HStack>
                             <HStack>
                                 <VStack alignItems="flex-start">
-                                    <label htmlFor="time">Time</label>
-                                    <Field name="time" type="number"/>
+                                    <label htmlFor="occasion">Occasion</label>
+                                    <Field name="occasion" as="select">
+                                        <option value="birthday">Birthday</option>
+                                        <option value="anniversary">Anniversary</option>
+                                        <option value="other">Other</option>
+                                    </Field>
                                 </VStack>
                                 <VStack alignItems="flex-start">
                                     <label htmlFor="guests">Guests</label>
-                                    <Field name="guests" type="number"/>
+                                    <Field name="guests" as="select">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                    </Field>
                                 </VStack>
-                            </HStack>
-                            <HStack>
+                                <VStack alignItems="flex-start">
+                                    <label htmlFor="time">Time</label>
+                                    <Field name="time" as="select">
+                                        <option value="5:00">5:00pm</option>
+                                        <option value="5:15">5:15pm</option>
+                                        <option value="5:30">5:30pm</option>
+                                        <option value="5:45">5:45pm</option>
+                                        <option value="6:00">6:00pm</option>
+                                        <option value="6:15">6:15pm</option>
+                                        <option value="6:30">6:30pm</option>
+                                        <option value="6:45">6:45pm</option>
+                                        <option value="7:00">7:00pm</option>
+                                        <option value="7:15">7:15pm</option>
+                                        <option value="7:30">7:30pm</option>
+                                        <option value="7:45">7:45pm</option>
+                                        <option value="8:00">8:00pm</option>
+                                        <option value="8:15">8:15pm</option>
+                                        <option value="8:30">8:30pm</option>
+                                        <option value="8:45">8:45pm</option>
+                                        <option value="9:00">9:00pm</option>
+                                        <option value="9:15">9:15pm</option>
+                                        <option value="9:30">9:30pm</option>
+                                        <option value="9:45">9:45pm</option>
+                                        <option value="10:00">10:00pm</option>
+                                    </Field>
+                                </VStack>
                                 <VStack alignItems="flex-start">
                                     <label htmlFor="date">Date</label>
                                     <DatePickerField name="date" />
                                 </VStack>
-                                <Square>
-                                <button type="submit">Submit</button>
-                                </Square>
                             </HStack>
+                            <Button type="submit" width="33%" height="50px" borderRadius={16}>Reserve a table</Button>
                         </VStack>
                     </Form>
+                    )}
                 </Formik>
             </VStack>
         </Container>
 )
 
 }
-
-//    Date & time
-
-//Number of guests
-
-//Occasion (Birthday, Anniversary)
-
-//Submit reservation button (to submit the form)
-
-//https://chakra-ui.com/docs/components/form-control
