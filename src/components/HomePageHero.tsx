4f589931081bd1a06ca664a8ed3fff3ea25cbcd1
Benjamin Elliott <benjamin.elliott.be@gmail.com>
2023-09-01 20:01:17 -0400

    changed layout of home page

--- src/components/HomePageHero.tsx
+++ src/components/HomePageHero.tsx
@@ -4,12 +4,14 @@ export const Hero = () => {
     return (
         <article className="hero">
             <div className="hero-text">
-                <div>
                     <h1 className='title title-display'>Little Lemon</h1>
                     <h2 className="title title-sub">Chicago</h2>
-                </div>
                 <p className="text text-paragraph">We are a family owned Mediterranean restaurant, focused on traditional recipes & served with a modern twist.</p>
-                <Link to={"/booking"}><button className="buttonh1">Reserve a table</button></Link>
+                <div className="hero-buttons">
+                    <Link to={"/about"}><button className="buttonh1">Read our story</button></Link>
+                    <Link to={"/menu"}><button className="buttonh1">Checkout our menu</button></Link>
+                    <Link to={"/booking"}><button className="buttonh1">Reserve a table</button></Link>
+                </div>
             </div>
             <img className="hero-image" src="restauranfood.jpg" alt="restaurant 1" />
         </article>
